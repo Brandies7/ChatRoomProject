36529ad6256cd70a17803fc1cb9e963fc48ff2e1
tgood111 <tgood111@live.com>
2017-08-24 18:21:02 -0500

    commented client class

--- Client/Client.cs
+++ Client/Client.cs
@@ -12,37 +12,42 @@ namespace Client
     {
         TcpClient clientSocket;
         NetworkStream stream;
+
         public Client(string IP, int port)
         {
+            // Instantiating a TcpClient
             clientSocket = new TcpClient();
-            clientSocket.Connect(IPAddress.Parse("127.0.0.1"), 9999);
+
+            // We are connecting it to a server with IP = IP and port = port
+            clientSocket.Connect(IPAddress.Parse(IP), port);
+
+            // Open a stream connection between client and server.
             stream = clientSocket.GetStream();
-            
         }
+
         public void Send()
         {
-            while(true)
-            {
-                string messageString = UI.GetInput();
-                byte[] message = Encoding.ASCII.GetBytes(messageString);
-                stream.Write(message, 0, message.Count());
-            }
+            // Get a message.
+            string messageString = UI.GetInput();
+
+            // Convert the message to bytes (1s and 0s).
+            byte[] message = Encoding.ASCII.GetBytes(messageString);
+
+            // Send the message over the network stream
+            stream.Write(message, 0, message.Count());
 
         }
+
         public void Recieve()
         {
-            while (true)
-            {
-                byte[] recievedMessage = new byte[256];
-                stream.Read(recievedMessage, 0, recievedMessage.Length);
-                UI.DisplayMessage(Encoding.ASCII.GetString(recievedMessage));
-            }
-<<<<<<< HEAD
-=======
-            
-            
->>>>>>> ca128099f06fdafb09e41891097494e5fc142f6d
+            // Creating a byte array with length 256.
+            byte[] recievedMessage = new byte[256];
+
+            // Reads 256 bytes of any message being received.
+            stream.Read(recievedMessage, 0, recievedMessage.Length);
+
+            // Displays the message received into the console.
+            UI.DisplayMessage(Encoding.ASCII.GetString(recievedMessage));
         }
-            
     }
 }
