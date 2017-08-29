e5c3fbe9055788f30736112829ded14363eecf78
tgood111 <tgood111@live.com>
2017-08-28 22:37:57 -0500

    added interface,log file,queue, modified code

---
+++ Server/textlog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Server
+{
+    class Consolelog : ILog
+
+{
+    public string message;
+
+
+    public void RecieveMessage(string message)
+    {
+        this.message = message;
+    }
+
+    public void SaveMessage()
+    {
+        Console.WriteLine(message);
+    }
+}
+}
